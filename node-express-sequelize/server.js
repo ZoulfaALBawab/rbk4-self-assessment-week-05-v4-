1cc5c89429bc809c368133ee303a162b65743cac
ZoulfaALBawab <zoulfaalbawab90@gmail.com>
2018-04-14 09:58:41 +0300

    5th aseessment

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -9,10 +9,23 @@ var User = sequelize.define('User', {
 
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
+app.get('/users',function(req , res)
+{
+	if (!User)
+	{
+		console.error('The User is not existe!');
+	}
+	console.log();
+	res.send('User');
+});
 
-
+ // When i made a GET req from the server ,, the server will send to the DataBase ..
+// Then, i have to compare if the user is existe in my database to get the users i want ,, otherwise will be error ..  
 
 module.exports = { 
   app: app,
   User: User
 };
+
+
+ 
